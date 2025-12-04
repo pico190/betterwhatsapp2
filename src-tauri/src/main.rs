1f35621fd318091c3dfac016f7f2e4f916759703
pico190 <picoprograma190@proton.me>
2025-12-04 17:27:56 +0100

    ✨ Add WhatsApp icon SVG to project

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,19 +2,6 @@
 
 fn main() {
     tauri::Builder::default()
-        .setup(|app| {
-            // Create main window loading WhatsApp Web
-            tauri::webview::WebviewWindowBuilder::new(
-                app,
-                "main",
-                tauri::WebviewUrl::External("https://web.whatsapp.com".parse().unwrap()),
-            )
-            .title("BetterWhatsApp")
-            .inner_size(1200.0, 800.0)
-            .build()?;
-
-            Ok(())
-        })
         .build(tauri::generate_context!())
         .expect("error while building tauri application")
         .run(|_app, _event| {})
